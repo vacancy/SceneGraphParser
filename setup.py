e50ef7ecb6ed1fa60c963aa8e21f349f6f4dff91
Jiayuan Mao <maojiayuan@gmail.com>
2022-03-01 11:45:18 -0600

    [master] bump version 0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@
 # This file is part of SceneGraphParser.
 # Distributed under terms of the MIT license.
 
-
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -21,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2',
+    version='0.1.0',
 
     description='A python toolkit for parsing sentences (natural language) into scene graphs (symbolic representations).',
     long_description=long_description,
